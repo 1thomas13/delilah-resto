83aa792a1e62a21f70e23f6306d805771ef86d74
Thomas Barreto <thomi.b137@gmail.com>
2022-03-10 10:20:43 -0300

    credentials reds and redis

--- src/products/controllers/index.js
+++ src/products/controllers/index.js
@@ -1,79 +1,84 @@
-const repositories = require('../repositories/productsRepositories')
-const config = require("../../config")
+const repositories = require("../repositories/productsRepositories");
+const config = require("../../config");
 
-const redis = require("redis")
-const bluebird = require('bluebird')
-bluebird.promisifyAll(redis)
+const redis = require("redis");
+
+const bluebird = require("bluebird");
+
+bluebird.promisifyAll(redis);
+
 const client = redis.createClient({
-  host:config.config.redis.host,
-  port:config.config.redis.port
-})
-
+  host: config.config.redis.host,
+  port: config.config.redis.port,
+});
 
 exports.allProducts = async (req, res) => {
+  const cache = req.cache;
+  console.log(cache);
+  if (cache == null) {
+    const products = await repositories.getAll();
 
-  const cache = req.cache
-  console.log(cache)
-  if(cache == null){
-    const products = await repositories.getAll()
-    
-    client.set("products", JSON.stringify(products), 'EX', 60)
+    client.set("products", JSON.stringify(products), "EX", 60);
 
-    return res.status(200).json({ products: products})
+    return res.status(200).json({ products: products });
   }
-  
-  res.status(200).json({ products: JSON.parse(cache)})
-}
+
+  res.status(200).json({ products: JSON.parse(cache) });
+};
 
 exports.getProduct = async (req, res) => {
-  const productid = req.params.productid
-  
-  const product = await repositories.getOne(productid)
+  const productid = req.params.productid;
 
-  res.status(200).json({ product: product })
-}
+  const product = await repositories.getOne(productid);
+
+  res.status(200).json({ product: product });
+};
 
 exports.createProduct = async (req, res) => {
-  const { name, price, available } = req.body
+  const { name, price, available } = req.body;
 
   if (!name || !price) {
-    res.status(400).json({ message: 'Falta ingresar datos para crear un producto' })
+    res
+      .status(400)
+      .json({ message: "Falta ingresar datos para crear un producto" });
   }
 
   const newProduct = {
     name: name,
     price: price,
-    available: available
-  }
+    available: available,
+  };
 
-  await repositories.addProduct(newProduct)
-  client.del('products')
-  res.status(201).json({ message: 'Producto agregado!' })
-}
+  await repositories.addProduct(newProduct);
+  client.del("products");
+  res.status(201).json({ message: "Producto agregado!" });
+};
 
 exports.modifyProduct = (req, res) => {
-  const productid = req.params.productid
-  const { name, price, available } = req.body
+  const productid = req.params.productid;
+  const { name, price, available } = req.body;
 
   if (name == undefined || price == undefined) {
-    res.status(400).json({ message: 'Falta ingresar modificar un crear producto' })
+    res
+      .status(400)
+      .json({ message: "Falta ingresar modificar un crear producto" });
   }
 
   const modifiedProduct = {
     name: name,
     price: price,
-    available: available
-  }
+    available: available,
+  };
 
-  repositories.modifyProduct(productid, { modifiedProduct })
-  client.del('products')
-  res.status(200).json({ message: 'Producto modificado!' })
-}
+  repositories.modifyProduct(productid, { modifiedProduct });
+  client.del("products");
+  res.status(200).json({ message: "Producto modificado!" });
+};
 
 exports.deleteProduct = async (req, res) => {
-  const productid = req.params.productid
+  const productid = req.params.productid;
 
-  await repositories.deleteProduct(productid)
-  client.del('products')
-  res.status(200).json({ message: 'Producto eliminado!' })
-}
+  await repositories.deleteProduct(productid);
+  client.del("products");
+  res.status(200).json({ message: "Producto eliminado!" });
+};
