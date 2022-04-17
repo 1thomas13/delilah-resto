c56097014f77ad55be85324c279a43e5c90e4d85
Thomas Barreto <thomi.b137@gmail.com>
2022-04-17 02:36:11 -0300

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -17,29 +17,6 @@ exports.config = {
   redis:{
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT
-  },
-  authGoogle:{
-    clientID: process.env.ID_GOOGLE,
-    clientSecret: process.env.SECRET_GOOGLE,
-    callbackURL: process.env.CALLBACK_GOOGLE
-  },
-  authFacebook:{
-    clientID: process.env.ID_FACEBOOK,
-    clientSecret: process.env.SECRET_FACEBOOK,
-    callbackURL: process.env.CALLBACK_FACEBOOK
-  },
-  authLinkedin:{
-    clientID: process.env.ID_LINKEDIN,
-    clientSecret: process.env.SECRET_LINKEDIN,
-    callbackURL: process.env.CALLBACK_LINKEDIN
-  },
-  authGitHub:{
-    clientID: process.env.ID_GITHUB,
-    clientSecret: process.env.SECRET_GITHUB,
-    callbackURL: process.env.CALLBACK_GITHUB
-  },
-  expressSession:{
-    secret:  process.env.SECRET_EXPRESS_SESSION,
   }
 
 }
